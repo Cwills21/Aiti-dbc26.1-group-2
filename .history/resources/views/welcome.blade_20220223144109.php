a94c3ccaa8a7311c42893f96f4249119ab29947d
Amponsah Collins <76946304+Cwills21@users.noreply.github.com>
2022-03-30 22:20:45 -0800

    Removed history folder from repository

--- .history/resources/views/welcome.blade_20220223144109.php
+++
@@ -1,8 +0,0 @@
-@extends('layout.master')
-
-@section('page_title', 'Welcome')
-
-@section('content')
-<h1>You Are Welcome to My First Laravel Application</h1>
-<h3>Please click on Log in from the Nav Bar To Start</h3>
-@endsection
